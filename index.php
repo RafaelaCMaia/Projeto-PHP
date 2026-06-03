6c4669443ca5a9d03ae69bc5ed95bf0ed5ad761c
RafaelaCMaia <113452699+RafaelaCMaia@users.noreply.github.com>
2026-06-02 21:52:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo "Projeto PHP funcionando!";
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>AtendeLab</title>
+</head>
+<body>
+ <h1>AtendeLab</h1>
+ <p>Sistema de Controle de Atendimentos Acadêmicos.</p>
+</body>
+</html>
